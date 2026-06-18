a6353aaf5bb66cb0cd89f8e7a7b74b99f0b6b806
Maksym Dimekhin <dimehinm@gmail.com>
2026-06-18 01:09:26 +0100

    lightbox

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,11 +1,19 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const galler = document.querySelector(".gallery");
-const loader = document.querySelector(".loader");
-const button = document.querySelector(".loadMoreBtn");
-const galleryCard = ({largeImageURL,webformatURL,tags,likes,views,comments,downloads})=>{
-    return ` <li class="item">
+const galler = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const button = document.querySelector('.loadMoreBtn');
+const lightbox = new SimpleLightbox('.gallery > li > a');
+const galleryCard = ({
+  largeImageURL,
+  webformatURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) => {
+  return ` <li class="item">
               <a href=${largeImageURL}>
                 <img src=${webformatURL} alt=${tags}>
                 <div class="img_text_list">
@@ -15,27 +23,27 @@ const galleryCard = ({largeImageURL,webformatURL,tags,likes,views,comments,downl
                   <p><span>Downloads</span>${downloads}</p>
                 </div>
               </a>
-            </li>`
-}
+            </li>`;
+};
 const createGallery = images => {
- const markup = images.map(galleryCard).join("");
- galler.insertAdjacentHTML("beforeend", markup)
- new SimpleLightbox('.gallery > li > a').refresh();
+  const markup = images.map(galleryCard).join('');
+  galler.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 };
 const clearGallery = () => {
-    galler.innerHTML="";
+  galler.innerHTML = '';
 };
 const showLoader = () => {
-    loader.classList.remove("visually-hidden")
+  loader.classList.remove('visually-hidden');
 };
 const hideLoader = () => {
-    loader.classList.add("visually-hidden")
+  loader.classList.add('visually-hidden');
 };
 const showLoadMoreButton = () => {
-    button.classList.remove("visually-hidden")
+  button.classList.remove('visually-hidden');
 };
 const hideLoadMoreButton = () => {
-     button.classList.add("visually-hidden")
+  button.classList.add('visually-hidden');
 };
 
 export {
